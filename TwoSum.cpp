31cf766e292d91654f2daab834efdc43c2a25a33
Alexander Peterson <abp6609@gmail.com>
2026-07-25 23:31:00 -0400

    finished the first challenge. I will do 4 more

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 
-
-
-
+using namespace std;
 
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target){
-
+        int n = 0;
+        while (n < nums.size()){
+            for(int i = n + 1; i < nums.size(); i++){
+                if (nums[i] + nums[n] == target){
+                    return {n,i};
+                }
+            }
+            n++;
         }
-};
+        return {};
+    }     
+};  
